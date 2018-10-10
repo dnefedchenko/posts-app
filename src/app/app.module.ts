070411b044526f868c39c0b9f2a3057f1f66cb28
Dmitriy Nefedchenko <dnefedchenko@gmail.com>
2018-10-10 22:52:42 +0300

    Angular material theming added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {PostComponent} from './posts/post.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
 
 
 @NgModule({
@@ -12,7 +14,9 @@ import {PostComponent} from './posts/post.component';
     PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post.component.ts
+++ src/app/posts/post.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  template: `<div>Post component implementation goes here</div>`
+  template: `<div>
+    <button mat-button mat-raised-button color="primary">Load Posts</button>
+  </div>`
 })
 export class PostComponent implements OnInit {
   ngOnInit(): void {
