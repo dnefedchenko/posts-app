abc31b7e90303131d7ce36c7423cedbb5c1567b0
Dmitriy Nefedchenko <dnefedchenko@gmail.com>
2018-10-10 22:29:40 +0300

    Project template generated

---
+++ src/app/posts/post.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  template: `<div>Post component implementation goes here</div>`
+})
+export class PostComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
